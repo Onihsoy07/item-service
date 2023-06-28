8c6d9a946f709ce83d3e8fcf31392dc7bf2894f0
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-06-28 21:36:44 +0900

    BeanValidation 동작 확인

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -4,8 +4,11 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.thymeleaf.spring5.processor.SpringUErrorsTagProcessor;
+import org.hibernate.validator.constraints.Range;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter @Setter
@@ -13,8 +16,16 @@ import java.util.List;
 public class Item {
 
     private Long id;
+
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     private Boolean open; //판매 여부
